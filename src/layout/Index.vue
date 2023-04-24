48801b1e311a0a847f5bb6d7bc6ac593669d4544
Max Zhao <mdcw1103@gmail.com>
2023-04-24 23:58:59 +0800

    Initial commit

---
+++ src/layout/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+}
+</script>
+
+<style scoped>
+
+</style>
