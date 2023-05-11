9ca46ed4c11ba3ace72af33c58f3f2fc2576120c
Max Zhao <mdcw1103@gmail.com>
2023-05-11 17:31:23 +0800

    feat: show page select button fix count

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="pagination flex justify-center items-center space-x-2">
+    <div class="i-carbon:page-first color-orange cursor-pointer" />
     <div
-      v-for="pageNum in pageCount"
+      v-for="pageNum in selectPageButtonCount"
       :key="pageNum"
       class="cursor-pointer px-2 py-1 h-5 leading-3 text-3 border-1 border-orange border-solid rounded hover:bg-orange [&[active]]:bg-orange"
-      :active="pageNum === page ? true : null"
-      @click="handlePageClick(pageNum)"
+      :active="(pageNum + pageNumStep) === page ? true : null"
+      @click="handlePageClick(pageNum + pageNumStep)"
     >
-      {{ pageNum }}
+      {{ pageNum + pageNumStep }}
     </div>
+    <div class="i-carbon:page-last color-orange cursor-pointer" />
   </div>
 </template>
 
@@ -24,8 +26,34 @@ const emits = defineEmits<{
   (e: 'update:page', page: number): void
 }>()
 
+const currentPage = computed(() => props.page)
 const pageCount = computed(() => {
   return Math.ceil(props.total / props.size)
+})
+const MAX_PAGE_BUTTON_COUNT = 7
+const MAX_HALF_PAGE_BUTTON_COUNT = Math.ceil(MAX_PAGE_BUTTON_COUNT / 2)
+const selectPageButtonCount = computed(() => {
+  return pageCount.value > MAX_PAGE_BUTTON_COUNT ? MAX_PAGE_BUTTON_COUNT : pageCount.value
+})
+
+const pageNumStep = computed(() => {
+  if (pageCount.value > MAX_PAGE_BUTTON_COUNT) {
+    if (currentPage.value <= MAX_HALF_PAGE_BUTTON_COUNT) {
+      return 0
+    }
+    else {
+      const maxCenterPage = pageCount.value - MAX_HALF_PAGE_BUTTON_COUNT
+      if (currentPage.value > maxCenterPage) {
+        return maxCenterPage - MAX_HALF_PAGE_BUTTON_COUNT
+      }
+      else {
+        return currentPage.value - MAX_HALF_PAGE_BUTTON_COUNT
+      }
+    }
+  }
+  else {
+    return 0
+  }
 })
 
 function handlePageClick(page: number) {
