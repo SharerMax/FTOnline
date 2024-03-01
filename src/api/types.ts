aed04f822fd2503c02691daa640b51036f3d5209
Max Zhao <mdcw1103@gmail.com>
2024-03-01 17:34:12 +0800

    chore: format code

--- src/api/types.ts
+++ src/api/types.ts
@@ -7,10 +7,10 @@ export type SupportedProviderKey = UniversalProviderKey | ProviderKey
 export interface Provider<Key extends SupportedProviderKey = SupportedProviderKey> {
   key: Key
   name: string
-  queryVideoList(keyword: string, page: number, videoType: string): Promise<ApiResponse<VideoDetail>>
-  queryVideosDetail(ids: string): Promise<ApiResponse<VideoDetail>>
-  queryVideoBriefList(keyword: string, page: number): Promise<ApiResponseWithClass<VideoBrief>>
-  queryVideoTypes(): Promise<VideoType[]>
+  queryVideoList: (keyword: string, page: number, videoType: string) => Promise<ApiResponse<VideoDetail>>
+  queryVideosDetail: (ids: string) => Promise<ApiResponse<VideoDetail>>
+  queryVideoBriefList: (keyword: string, page: number) => Promise<ApiResponseWithClass<VideoBrief>>
+  queryVideoTypes: () => Promise<VideoType[]>
 }
 
 export interface Providers {
